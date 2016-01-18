f99aec7c5c2fc9a0f4738f858a9d3c3cf46f6173
Timothy Huesmann <timothy.huesmann@gmail.com>
2016-01-18 12:29:14 -0600

    Auto-login is now fully functional from the start of the app

--- Investr/MainVC.swift
+++ Investr/MainVC.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import Parse
 
 class MainVC: UIViewController {
     
@@ -27,10 +27,59 @@ class MainVC: UIViewController {
         let registerVC = self.storyboard?.instantiateViewControllerWithIdentifier("RegisterVC") as! RegisterVC
         self.navigationController?.pushViewController(registerVC, animated: true)
     }
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let username = defaults.objectForKey("username") as! String
+        let password = defaults.objectForKey("password") as! String
+        if(username != "" && password != "")
+        {
+            PFUser.logInWithUsernameInBackground(username, password: password) {
+                (user: PFUser?, error: NSError?) -> Void in
+                if user != nil
+                {
+                    //success
+                    let defaults = NSUserDefaults.standardUserDefaults()
+                    defaults.setObject(username, forKey: "username")
+                    defaults.setObject(password, forKey: "password")
+                    defaults.synchronize()
+                    
+                    let menuVC = self.storyboard?.instantiateViewControllerWithIdentifier("MenuVC") as! MenuVC
+                    self.navigationController?.pushViewController(menuVC, animated: true)
+                    // Do stuff after successful login.
+                    InvestrCore.currUser = username         //saves global username
+                    let query = PFUser.query()
+                    query!.whereKey("username", equalTo: InvestrCore.currUser)
+                    
+                    do{
+                        _ = try query!.findObjects()
+                    }
+                    catch
+                    {
+                        
+                    }
+                    
+                    
+                    InvestrCore.userID = PFUser.currentUser()!.objectId!    //saves global user objectId
+                    
+                }
+                else
+                {
+                    //Error
+                    let alert = UIAlertView()
+                    alert.title = "Login Error"
+                    alert.message = "Invalid Email/Password Combination, Please Try Again"
+                    alert.addButtonWithTitle("OK")
+                    alert.show()
+                    // The login failed. Check error to see why.
+                }
+
+        }
+        
         
         // Do any additional setup after loading the view.
+    }
     }
     
     override func didReceiveMemoryWarning() {
